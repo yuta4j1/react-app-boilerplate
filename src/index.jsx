620395bb4abc627d3af07b5598c62e6965d29878
Ozaki Yoshiaki <kascado.ys10@gmail.com>
2019-02-20 23:25:04 +0900

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,55 +1,14 @@
-import React from 'react'
-import {render} from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
-class InputText extends React.Component {
-    constructor(props) {
-        super(props)
-        this.content = props
-    }
-    render() {
-        return (
-            <div>
-                <input type="text" id="taskText" value={this.content} />
-            </div>
-        )
-    }
+class Hello extends React.Component {
+  greeting() {
+    return 'Hello, World!';
+  }
+
+  render() {
+    return <div>{this.greeting()}</div>;
+  }
 }
 
-class AddButton extends React.Component {
-    constructor() {
-        this.label = 'Add'
-    }
-    render() {
-        return (
-            <div>
-                <button className="aButton">{ this.label }</button>
-            </div>
-        )
-    }
-}
-class Parent extends React.Component {
-    greeting() {
-        return 'Hello, World!'
-    }
-    getAnyWord(word) {
-        return (
-            <h1>{ word }</h1>
-        )
-    }
-    render() {
-        return (
-            <div>
-                <div>{this.getAnyWord(greeting())}</div>
-                <div className="float-box">
-                    <InputText />
-                    <AddButton />
-                </div>
-            </div>
-        )
-    }
-}
-
-render(
-    <Parent />, 
-    document.getElementById('app')
-)
+render(<Hello />, document.getElementById('app'));
